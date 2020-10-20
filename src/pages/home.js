fd31f8d91ab378e8edcf415a36def23886fc230d
aldhairescobar <aldhair.escobar@gmail.com>
2020-10-19 23:24:04 -0500

    component tree - Global Styles - Footer Component - Accordion Component and Context

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { FaqsContainer } from "../containers/faqs";
 import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
 
 export default function Home() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
