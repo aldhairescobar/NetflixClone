3902fbb669ac774b0537b524e5f9d0aa67992ae4
aldhairescobar <aldhair.escobar@gmail.com>
2020-10-22 17:12:01 -0500

    build and style feature component for our header

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,21 +3,29 @@ import { FaqsContainer } from "../containers/faqs";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
-import { OptForm } from "../components";
+import { OptForm, Feature } from "../components";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email Address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </OptForm.Text>
-        </OptForm>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
