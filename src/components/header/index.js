e2aab9ec6edc89d59c17e6eef5de6d3b81945ea3
aldhairescobar <aldhair.escobar@gmail.com>
2020-10-24 20:28:05 -0500

    add Dropdown -profiles- to Header Component - Profiles to Browse Page Loading Spinner

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -14,6 +14,9 @@ import {
   Search,
   SearchIcon,
   SearchInput,
+  Picture,
+  Dropdown,
+  Profile,
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -83,4 +86,16 @@ Header.Search = function HeaderSearch({
       />
     </Search>
   );
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
 };
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from "react";
-import { Header } from "../components";
+import React, { useState, useContext, useEffect } from "react";
+import { Header, Loading } from "../components";
 import * as ROUTES from "../constants/routes";
 import { FirebaseContext } from "../context/firebase";
 import { SelectProfileContainer } from "./profiles";
@@ -18,8 +18,15 @@ export function BrowseContainer() {
     photoURL: "1",
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [user]);
+
   return profile.displayName ? (
     <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
       <Header src="joker1" dontShowOnSmallViewPort>
         <Header.Frame>
           <Header.Group>
@@ -45,7 +52,21 @@ export function BrowseContainer() {
             <Header.Search
               searchTerm={searchTerm}
               setSearchTerm={setSearchTerm}
-            ></Header.Search>
+            />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.Link>{user.displayName}</Header.Link>
+                </Header.Group>
+                <Header.Group>
+                  <Header.Link onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.Link>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
           </Header.Group>
         </Header.Frame>
         <Header.Feature>
